abbcf81eef1070d0704b50089c33c0ec6bd3ac62
praatts <127567514+praatts@users.noreply.github.com>
2024-12-02 16:27:43 +0100

    Practica vacía

---
+++ src/tresenraya/juegotresenraya.java
@@ -0,0 +1,16 @@
+package tresenraya;
+import java.util.Scanner;
+import java.util.Random;
+public class juegotresenraya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+	}
+
+}
